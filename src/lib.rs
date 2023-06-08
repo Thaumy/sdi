68a188a7bb1c31b88d7147ceae168d51fc4dac79
Thaumy <thaumy@outlook.com>
2023-06-08 08:09:49 +0800

    add friendly err msg

--- src/lib.rs
+++ src/lib.rs
@@ -11,38 +11,98 @@ lazy_static! {
         Mutex::new(HashMap::new());
 }
 
+/// Register an statically resolved service expression indexed by key.
+///
+/// ## Example
+///
+/// ```rust
+/// use sdi::{inject, provide};
+///
+/// #[derive(Debug, PartialEq)]
+/// struct A;
+///
+/// impl A {
+///     pub fn new() -> A { A }
+///     provide!(A <- A::new());
+/// }
+///
+/// assert_eq!(A::new(), inject!(A))
+/// ```
+/// Provide by inject is also ok.
+/// ```rust
+/// use sdi::{inject, provide};
+///
+/// #[derive(Debug, PartialEq)]
+/// struct A;
+/// provide!(A <- A);
+///
+/// #[derive(Debug, PartialEq)]
+/// struct B(A);
+///
+/// impl B {
+///     pub fn new(a:A) -> B { B(a) }
+///     provide!(B <- B::new(inject!(A)));
+/// }
+///
+/// assert_eq!(B::new(A), inject!(B))
+/// ```
 #[proc_macro]
 pub fn provide(attr: TokenStream) -> TokenStream {
-    let (token, expr) = {
+    let (key, expr) = {
         let stringify = attr.to_string();
-        let mut split = stringify.split("by");
+        let split: Vec<String> = stringify
+            .split(" <- ")
+            .map(|str| str.to_owned())
+            .collect();
 
-        let token = split
-            .next()
-            .unwrap()
-            .trim()
+        let err_msg = "Invalid provider syntax, consider: provide!(<key> <- <expr>)";
+        if split.len() != 2 {
+            panic!("{}", err_msg);
+        }
+
+        let key = split
+            .get(0)
+            .expect(err_msg)
             .to_owned();
         let expr = split
-            .next()
-            .unwrap()
-            .trim()
+            .get(1)
+            .expect(err_msg)
             .to_owned();
 
-        (token, expr)
+        (key, expr)
     };
+
     MAP.lock()
         .unwrap()
-        .insert(token, expr);
+        .insert(key, expr);
+
     TokenStream::new()
 }
 
+/// Get an statically resolved service expression by key
+///
+/// ## Example
+///
+/// ```rust
+/// use sdi::{inject, provide};
+///
+/// #[derive(Debug, PartialEq)]
+/// struct A;
+///
+/// impl A {
+///     pub fn new() -> A { A }
+///     provide!(A <- A::new());
+/// }
+///
+/// assert_eq!(A::new(), inject!(A))
+/// ```
 #[proc_macro]
 pub fn inject(attr: TokenStream) -> TokenStream {
-    let stringify = attr.to_string();
+    let key = attr.to_string();
     MAP.lock()
         .unwrap()
-        .get(&stringify)
-        .unwrap()
+        .get(&key)
+        .expect(&format!("Unknown inject key: `{}`", key))
         .parse()
         .unwrap()
 }
